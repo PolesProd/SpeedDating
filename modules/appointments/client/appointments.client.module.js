d1002282777a9826677951792e7ffec4b11bd48d
Abdou00 <abdoulaye_diarra@outlook.fr>
2017-03-14 16:53:36 +0100

    Refactoring module réunions

--- modules/appointments/client/appointments.client.module.js
+++
@@ -1,8 +0,0 @@
-(function (app) {
-  'use strict';
-
-  app.registerModule('appointments');
-  app.registerModule('appointment', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
-  app.registerModule('appointment.services');
-  app.registerModule('appointment.routes', ['ui.router', 'core.routes', 'appointment.services']);
-}(ApplicationConfiguration));
